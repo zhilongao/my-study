363b4bfd1016cc9be0ca5688d86bda6d818575ef
gzl <303547207@qq.com>
2020-11-15 08:21:55 +0800

    【add】rabbitmq添加与普通spring项目的集成。

---
+++ study-normal-spring/src/main/java/com/study/mq/rabbitmq/consumer/FirstConsumer.java
@@ -0,0 +1,15 @@
+package com.study.mq.rabbitmq.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class FirstConsumer implements MessageListener{
+
+    private Logger logger = LoggerFactory.getLogger(FirstConsumer.class);
+
+    public void onMessage(Message message) {
+        logger.info("The first consumer received message : " + message.getBody());
+    }
+}
