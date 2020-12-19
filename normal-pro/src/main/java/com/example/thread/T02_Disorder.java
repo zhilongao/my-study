6ddc748791f92998da905e83fa60c11267fd3351
gzl <303547207@qq.com>
2020-12-19 08:44:06 +0800

    【add】.......

---
+++ normal-pro/src/main/java/com/example/thread/T02_Disorder.java
@@ -0,0 +1,43 @@
+package com.example.thread;
+
+public class T02_Disorder {
+
+    private static int x = 0, y = 0;
+
+    private static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        for (;;){
+            i++;
+            x = 0; y = 0;
+            a = 0; b = 0;
+            Thread one = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    a = 1;
+                    x = b;
+                }
+            });
+            Thread two = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    b = 1;
+                    y = a;
+                }
+            });
+            one.start();
+            two.start();
+            one.join();
+            two.join();
+            String result = "第" + i + "次(" + x + "," + y + ")";
+            if (x == 0 && y == 0) {
+                System.err.println(result);
+                break;
+            } else {
+
+            }
+        }
+    }
+
+}
