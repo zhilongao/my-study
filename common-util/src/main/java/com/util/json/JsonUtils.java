690a510e0a8c8d19ca78976f467610ac832b212b
gzl <303547207@qq.com>
2022-01-20 14:57:01 +0800

    【add】elk日志收集。

--- common-util/src/main/java/com/util/json/JsonUtils.java
+++ common-util/src/main/java/com/util/json/JsonUtils.java
@@ -1,11 +1,28 @@
 package com.util.json;
 
 import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class JsonUtils {
 
+    private static int jsonType = 1;
+
+    private static ObjectMapper mappers = new ObjectMapper();
+
     public static String toJson(Object obj) {
-        return JSONObject.toJSONString(obj);
+        String result = "";
+        if (1 == jsonType) {
+            return JSONObject.toJSONString(obj);
+        } else if (2 == jsonType) {
+            try {
+                return mappers.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
 }
