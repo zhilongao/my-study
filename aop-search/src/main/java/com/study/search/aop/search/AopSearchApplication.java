35a04eb345b3ec0a605a735c6131d6dfd04f51b8
gaozhilong <zhilong_gao@kuaidi100.com>
2022-03-18 18:47:03 +0800

    日志demo1

--- aop-search/src/main/java/com/study/search/aop/search/AopSearchApplication.java
+++ aop-search/src/main/java/com/study/search/aop/search/AopSearchApplication.java
@@ -1,15 +1,30 @@
 package com.study.search.aop.search;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AopSearchApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(AopSearchApplication.class, args);
+//    }
+
+
     public static void main(String[] args) {
-        SpringApplication.run(AopSearchApplication.class, args);
+        Map<String, String> map = new HashMap<>();
+        map.put("kuaidiCom", "shentong");
+        map.put("sendAddress", "广东云浮市罗定市罗城街道");
+        map.put("recAddress", "广东云浮市罗定市罗城街道");
+        map.put("weight", "1");
+        map.put("serviceType", "");
+        System.err.println(JSONObject.toJSONString(map));
     }
 }
